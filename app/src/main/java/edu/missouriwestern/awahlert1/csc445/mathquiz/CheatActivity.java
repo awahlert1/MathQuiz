564ab75ae26308d932748704e9db8a0cdc169998
Austin Wahlert <awahlert1@missouriwestern.edu>
2017-02-15 14:50:23 -0600

    Fixed previous button causing crash when pressed on question 1. now the previous and next button cycle through all questions

---
+++ app/src/main/java/edu/missouriwestern/awahlert1/csc445/mathquiz/CheatActivity.java
@@ -0,0 +1,13 @@
+package edu.missouriwestern.awahlert1.csc445.mathquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CheatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+    }
+}
